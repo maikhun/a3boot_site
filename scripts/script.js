73df648c6554a055a6905aca39b1fddc99283418
Anton <62674062+maikhun@users.noreply.github.com>
2022-12-12 11:52:47 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+'use strict'
+
+document.getElementById('main-action-first').onclick = function () {
+    document.getElementById('jobs').scrollIntoView({behavior: 'smooth'});
+}
+
+document.getElementById('btn').onclick = function () {
+    
+    // const fio = document.getElementById('fio');
+    // const email = document.getElementById("mail");
+    // var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    // if (typeof fio != "string") {
+    //     alert("Введите корректное имя пользователя");
+    // }
+    // else if (typeof fio != "string" && !reg.test(email)){
+    //     alert("Введите корректный адрес почты");
+    // }
+    // else if(typeof fio === "string" && reg.test(mail)){
+    //     alert(`Мы сообщим вам на почту ${mail} информацию о вакансиях`);
+    // }
+
+}
+
+document.getElementById('main-action-second').onclick = function () {
+
+}
+
+
+
+document.getElementById('response-action').onclick = function () {
+}
+
